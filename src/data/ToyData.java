95f0a780a0362619343c29b15856e36d30572c3d
itsmeny <chokjaroenp501@gmail.com>
2020-11-10 17:50:48 +0700

    v4

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package data;
 
 import base.Bike;
@@ -12,76 +12,20 @@ public class ToyData {
         bikes[0] = new Bike ("Volvo" , 255);
         bikes[1] = new Bike ("Vigo" , 256);
         bikes[2] = new Bike ("Toyota" , 257);
-      
+      //test
         return bikes;
     }
 
     public static Person[] getPerson() {
         Person[] person = new Person[3];
-        // your code
-
-        return person;
-    }
-
-    public static Parcel[] getParcels() {
-        Parcel[] parcels = new Parcel[10];
-        // your code
-
-        return parcels;
-    }
-
-    //testing
-    public static void main(String[] args) {
-        printBike();
-        printPerson();
-        printParcels();
-    }
-
-    private static void printBike() {
-        Bike[] bikes = getBikes();
-        for (int i = 0; i < bikes.length; i++) {
-            System.out.println(bikes[i].toString());
-        }
-    }
-
-    private static void printPerson() {
-        Person[] person = getPerson();
-        for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].toString());
-        }
-    }
-
-    private static void printParcels() {
-        Parcel[] percels = getParcels();
-        for (int i = 0; i < percels.length; i++) {
-            System.out.println(percels[i].toString());
-        }
-    }
-
-}
-=======
-package data;
-
-import base.Bike;
-import base.Parcel;
-import base.Person;
-
-public class ToyData {
-
-    public static Bike[] getBikes() {
-        Bike[] bikes = new Bike[3];
-        // your code
-
-        return bikes;
-    }
-
-    public static Person[] getPerson() {
-        Person[] person = new Person[3];
+       
         person[0] = new Person("Jack" , "Black");
         person[1] = new Person("Mark" , "Trump");
         person[2] = new Person("Max" , "Smith");
         return person;
-        return person;
+        // your code
+
+        
     }
 
     public static Parcel[] getParcels() {
@@ -120,4 +64,4 @@ public class ToyData {
     }
 
 }
->>>>>>> b6e7fab2085cd15b1e91f72d4edc4e3fed1beeef
+
