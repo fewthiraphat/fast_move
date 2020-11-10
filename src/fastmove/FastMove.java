836b5470d49e049b545c911beaa9ac881e49208b
itsmeny <chokjaroenp501@gmail.com>
2020-11-10 16:58:08 +0700

    v1

--- src/fastmove/FastMove.java
+++ src/fastmove/FastMove.java
@@ -1,12 +1,13 @@
 package fastmove;
 
 import base.Person;
-
+import data.ToyData;
 
 public class FastMove {
     
     private FastBike[] fastBikes;
     private Person[] person;
-
+     
+   
     
 }
